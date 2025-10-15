48ecd3afb9dbd43c15f45dacd1caa42fd29c7e5c
PiroMonarch <prakash9045880087@gmail.com>
2025-10-15 18:01:41 +0530

    updted codee

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 const express = require('express'); 
 const app = express();
+const mongoose = require("mongoose")
+const path = require('path');
+const chat = require('./models/chat.js');
+
+
+
+
+
+main().then(()=> console.log ('Connected to MongoDB'))
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
 
 app.get("/", (_req, res) => {
     res.send('Hello, World!');
@@ -8,3 +24,4 @@ app.get("/", (_req, res) => {
 app.listen(8080, () => {
     console.log('Server is listening on port 8080');
 });
+
--- init.js
+++ init.js
@@ -1,16 +1,6 @@
 const mongoose = require('mongoose');
 
-main().then(()=> console.log ('Connected to MongoDB'))
-.catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
-}
-
-const chat = require('./models/chat.js');
-
-
-
+const Chat = require('./models/chat.js');
 
 let allchats = [
     {
@@ -43,10 +33,5 @@ let allchats = [
         message: "Happy Birthday!",
         created_at: new Date()
     }
-];
-
-Chat.insertMany(allchats)
-    .then(() => console.log("Chats inserted"))
-    .catch(err => console.log(err));
-
-    
+]
+chat.insertMany(allchats);
